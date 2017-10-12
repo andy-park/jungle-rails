394a4f08794f3d3aad48567840d7416eddb40a47
Andrew Park <andresparque@gmail.com>
2017-10-12 20:29:17 +0000

    Added a category index

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,10 +4,14 @@ class Admin::CategoriesController < ApplicationController
         @category = Category.new
     end
 
+    def index
+        @categories = Category.order(id: :desc).all
+    end
+
     def create
         @category = Category.new category_params
         if @category.save
-            redirect_to '/'      
+            redirect_to '/admin/categories'      
         else 
             render :new
         end
