fbf4f0356dc84fa564ee59bc3819170ab37e58cf
Andrew Park <andresparque@gmail.com>
2017-10-13 01:39:39 +0000

    Added admin security

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+    
+    before_filter :authenticate
 
     def new
         @category = Category.new
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["MY_USER"] && password == ENV["MY_PASS"]
+    end
+  end
+
   private
 
   def current_user
